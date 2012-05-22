2a5e3ed733e5283927a1185fef767ee2714c62d0
Pavel Perestoronin <eigenein@gmail.com>
2012-05-22 05:40:29 +0300

    First

--- handlers/first.py
+++ handlers/first.py
@@ -7,6 +7,6 @@ class FirstHandler:
     def GET(self):
         nickname = web.input(nickname=None).nickname
         if nickname is not None:
-            return web.ctx.render.jockeys(web.ctx.jockeys.get_jockeys_by_horse_nickname(nickname))
+            return web.ctx.render.first_results(web.ctx.jockeys.get_jockeys_by_horse_nickname(nickname))
         return web.ctx.render.first()
 
